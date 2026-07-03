a52d191ea9f50810a58aabdc67bd5cd0f61905d0
董锡文 <saurick13794566255@gmail.com>
2026-07-03 22:04:08 +0800

    feat(config): expand runtime field policy surfaces

---
+++ server/internal/biz/customer_config_field_policy_surfaces_runtime.go
@@ -0,0 +1,82 @@
+package biz
+
+func init() {
+	registerRuntimeFieldPolicySurface("sales_order_items.default", []string{
+		"product_no",
+		"product_name",
+		"sku_no",
+		"quantity",
+		"unit_price",
+		"amount",
+	})
+	registerRuntimeFieldPolicySurface("purchase_orders.default", []string{
+		"purchase_order_no",
+		"supplier_snapshot",
+		"expected_arrival_date",
+		"lifecycle_status",
+	})
+	registerRuntimeFieldPolicySurface("purchase_order_items.default", []string{
+		"product_order_no",
+		"product_no",
+		"material_name",
+		"spec",
+		"unit",
+		"quantity",
+		"unit_price",
+		"amount",
+	})
+	registerRuntimeFieldPolicySurface("purchase_receipts.default", []string{
+		"receipt_no",
+		"purchase_order_no",
+		"warehouse",
+		"status",
+	})
+	registerRuntimeFieldPolicySurface("quality_inspections.default", []string{
+		"inspection_no",
+		"source_no",
+		"result",
+		"decision_note",
+	})
+	registerRuntimeFieldPolicySurface("inventory_lots.default", []string{
+		"lot_no",
+		"warehouse",
+		"material",
+		"quantity",
+		"status",
+	})
+	registerRuntimeFieldPolicySurface("inventory_txns.default", []string{
+		"txn_no",
+		"txn_type",
+		"warehouse",
+		"material",
+		"quantity",
+	})
+	registerRuntimeFieldPolicySurface("shipments.default", []string{
+		"shipment_no",
+		"sales_order_no",
+		"warehouse",
+		"status",
+		"shipped_at",
+	})
+	registerRuntimeFieldPolicySurface("outsourcing_orders.default", []string{
+		"outsourcing_order_no",
+		"processor_snapshot",
+		"return_date",
+		"lifecycle_status",
+	})
+	registerRuntimeFieldPolicySurface("finance_facts.default", []string{
+		"finance_fact_no",
+		"source_no",
+		"fact_type",
+		"amount",
+		"status",
+	})
+}
+
+func registerRuntimeFieldPolicySurface(surfaceKey string, fieldKeys []string) {
+	fields := map[string]struct{}{}
+	for _, fieldKey := range fieldKeys {
+		fields[fieldKey] = struct{}{}
+	}
+	runtimeFieldPolicySurfaceKeys[surfaceKey] = fields
+}
